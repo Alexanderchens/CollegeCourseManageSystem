b726dd3caf0ec3e77b095a8aff7f254ceab13e22
Gary-code <1620216938@qq.com>
2021-04-15 20:55:54 +0800

    学生端路由路径

--- fontend/src/router.js
+++ fontend/src/router.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './view/Login/Login'
 import Home from './view/student/StuHome'
+import Message from './view/student/eduMessage/Message'
+import NotFound from './view/page404'
+import Table from './view/student/class/table'
+import Query from './view/student/class/query'
+import Select from './view/student/select/select'
+import Classroom from './view/student/classroom/classroom'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -16,7 +22,22 @@ const router = new VueRouter({
     },
     {
       path: '/student',
-      component: Home
+      component: Home,
+      redirect: '/message',
+      children: [
+        { path: '/message', component: Message },
+        { path: '/table', component: Table },
+        { path: '/query', component: Query },
+        { path: '/select', component: Select },
+        { path: '/classroom', component: Classroom }
+      ]
+    },
+    {
+      path: '/404',
+      component: NotFound,
+      name: '404',
+      hidden: true,
+      children: []
     }
   ]
 })

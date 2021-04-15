b5ac47341e4ec4b9aa1286ef75594a5093c90357
Gary-code <1620216938@qq.com>
2021-04-15 20:57:23 +0800

    主页

--- fontend/src/view/student/StuHome.vue
+++ fontend/src/view/student/StuHome.vue
@@ -6,63 +6,84 @@
         <img src="../../assets/logo.png" alt="" class="logo_pic">
         <span>选课管理系统-学生端</span>
       </div>
-      <el-button type="warning" @click="logout" circle icon="el-icon-turn-off">注销</el-button>
+      <el-dropdown>
+        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>注销</el-dropdown-item>
+          <el-dropdown-item divided>其他业务1</el-dropdown-item>
+          <el-dropdown-item>其他业务2</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </el-header>
 <!--    主体-->
     <el-container>
 <!--      Aside-->
       <el-aside width="200px">
         <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
+          :default-active="$route.path"
+          class="el-menu-vertical-aside"
           @open="handleOpen"
           @close="handleClose"
+          :collapse="isCollapse"
           background-color="#03152a"
-          text-color="#fff">
+          text-color="#fff"
+          router
+          @click="saveNavState('/')">
           <el-submenu index="1">
             <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <i class="el-icon-message"></i>
+              <span>教务信息</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item :index="'/message'"><i class="el-icon-chat-line-square"></i>消息</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-collection"></i>
+              <span>课程查询</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item :index="'/table'"> <i class="el-icon-s-grid"></i>学生课表</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item :index="'/query'"><i class="el-icon-info"></i>选课情况查询</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-menu-item :index="'/select'">
             <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
+            <span slot="title">学生选课</span>
           </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
+          <el-menu-item :index="'/classroom'">
+            <i class="el-icon-question"></i>
+            <span slot="title">查询空教室</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
       <el-container>
 <!--        Main-->
-        <el-main>Main</el-main>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
 <!--        Footer-->
-        <el-footer>Footer</el-footer>
+        <el-footer>
+          <div>
+            <p class="comment">Copyright © 2021 数据库实训小组.</p>
+          </div>
+        </el-footer>
       </el-container>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      isCollapse: false
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -89,4 +110,30 @@ export default {}
     width: 50px;
     border-radius: 50%;
   }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+
+  .el-menu-vertical-aside {
+    border: none;
+  }
+
+  .el-menu-vertical-aside:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .comment{
+    height: 60px;
+    line-height: 60px;
+    text-align: left;
+    font-size: 20px;
+    color: #999999;
+  }
+
 </style>

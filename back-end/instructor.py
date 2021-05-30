9dc9444f029d0f442e3e6cd8abf2312923b4da72
SCUT-Kyle <2537496966@qq.com>
2021-05-30 12:03:58 +0800

    Update instructor.py

--- back-end/instructor.py
+++ back-end/instructor.py
@@ -23,3 +23,23 @@ def querystudentlist(c_id):
     db.close()
     return s_list
 
+#老师挑选学生，前端返回该课程最终学生列表，插入数据库
+def confirmstudent(s_list):
+    db = pms.connect(host='localhost', user='root', passwd='root', db='collegecoursemanagesystem', charset='utf8')
+    mcs = db.cursor()
+    insert_str = "insert into student_pick(s_id, c_id, name, password)values( % s, % s, %s, %s)"
+    for item in s_list:
+        mcs.execute(insert_str,(s_list[i].s_id, s_list[i].c_id,s_list[i].name, s_list[i].password))
+    db.commit()
+    mcs.close()
+    db.close()
+
+#老师填写课程信息，插入待审核课表
+def submitcourseinformation(c_id,course_name,credits,type,hour,dept_name):
+    db = pms.connect(host='localhost', user='root', passwd='root', db='collegecoursemanagesystem', charset='utf8')
+    mcs = db.cursor()
+    insert_str = "insert into course(c_id,course_name,credits,type,hour,dept_name)values( % s, % s, %s, %s, %s, %s)"
+    mcs.execute(insert_str,(c_id,course_name,credits,type,hour,dept_name))
+    db.commit()
+    mcs.close()
+    db.close()
